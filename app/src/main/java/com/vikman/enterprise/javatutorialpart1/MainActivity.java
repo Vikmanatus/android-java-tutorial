7b70083018099775f50faf39d1e51bcaa7f9bdda
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-06-21 22:06:20 +0200

    basic java screen and user handling

--- app/src/main/java/com/vikman/enterprise/javatutorialpart1/MainActivity.java
+++ app/src/main/java/com/vikman/enterprise/javatutorialpart1/MainActivity.java
@@ -3,12 +3,23 @@ package com.vikman.enterprise.javatutorialpart1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView welcomeTextView;
+    private EditText editTextTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        welcomeTextView = findViewById(R.id.welcomeMessage);
+        editTextTitle = findViewById(R.id.editTextTextPersonName);
+    }
+    public void onBtnClick(View view){
+        welcomeTextView.setText(editTextTitle.getText().toString());
+        editTextTitle.setText("");
     }
 }
