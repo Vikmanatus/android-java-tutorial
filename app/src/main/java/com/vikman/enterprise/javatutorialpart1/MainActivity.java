2f2ccb924b3c331fdb6443ad6e53e8ab29f16cc6
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-06-26 11:15:26 +0200

    Beginning UI basics tutorial

--- app/src/main/java/com/vikman/enterprise/javatutorialpart1/MainActivity.java
+++ app/src/main/java/com/vikman/enterprise/javatutorialpart1/MainActivity.java
@@ -9,37 +9,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText editTextFirstname;
-    private EditText editTextLastname;
-    private EditText editTextEmail;
-    private Button registerButton;
-    private TextView firstName;
-    private TextView lastName;
-    private TextView email;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextLastname = findViewById(R.id.editTextLastname);
-        editTextFirstname = findViewById(R.id.editTextFirstname);
-        registerButton = findViewById(R.id.registerButton);
-        firstName = findViewById(R.id.firstNameTextView);
-        lastName = findViewById(R.id.lastNameTextView);
-        email = findViewById(R.id.emailTextView);
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firstName.setText("First Name: "+editTextFirstname.getText().toString());
-                lastName.setText("First Name: "+editTextLastname.getText().toString());
-                email.setText("First Name: "+editTextEmail.getText().toString());
-                editTextFirstname.setText("");
-                editTextLastname.setText("");
-                editTextEmail.setText("");
-            }
-        });
     }
 }
